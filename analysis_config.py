9ba1aece6780c25fbfea8575c2f588defe394a2e
djordje <djordje5996@gmail.com>
2026-03-09 20:59:27 +0100

    moved analysis config into separate file

---
+++ analysis_config.py
@@ -0,0 +1,45 @@
+analysis_config = {
+    "WRFZO": {
+        "naziv": "Sekvenciranje celog egzoma",
+        "sifra": "039",
+        "geni": "svih",
+        "panel": "Exome 2.0 (Illumina)",
+        "ograda": "",
+        "instrument": "NextSeq 2000 (Illumina)"
+        
+    },
+    "pWES": {
+        "naziv": "Sekvenciranje celog egzoma",
+        "sifra": "040",
+        "geni": "svih",
+        "panel": "Exome 2.0 (Illumina)",
+        "ograda": "",
+        "instrument": "NextSeq 2000 (Illumina)"
+        
+    },
+    "RFZO": {
+        "naziv": "Sekvenciranje kliničkog egzoma",
+        "sifra": "037",
+        "geni": "4813 klinički relevantnih",
+        "panel": "TruSight One (Illumina)",
+        "ograda": "Geni koji se ne nalaze na TSO panelu nisu analizirani. ",
+        "instrument": "NextSeq 550 (Illumina)"
+        
+    },
+    "SOVO": {
+        "naziv": "Sekvenciranje kliničkog egzoma",
+        "sifra": "037-SOVO",
+        "geni": "4813 klinički relevantnih",
+        "panel": "TruSight One (Illumina)",
+        "ograda": "Geni koji se ne nalaze na TSO panelu nisu analizirani. ",
+        "instrument": "NextSeq 550 (Illumina)"
+    },
+    "NGS": {
+        "naziv": "Sekvenciranje kliničkog egzoma",
+        "sifra": "018",
+        "geni": "4813 klinički relevantnih",
+        "panel": "TruSight One (Illumina)",
+        "ograda": "Geni koji se ne nalaze na TSO panelu nisu analizirani. ",
+        "instrument": "NextSeq 550 (Illumina)"
+    }
+}
